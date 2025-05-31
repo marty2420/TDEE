e777dc2ade1f9b78dd3feab6055401f429bac38b
marty2420 <104203147+marty2420@users.noreply.github.com>
2025-05-31 23:40:03 +0800

    Update TdeeCalculator.tsx

--- src/components/calculator/TdeeCalculator.tsx
+++ src/components/calculator/TdeeCalculator.tsx
@@ -53,8 +53,8 @@ const TdeeCalculator: React.FC = () => {
 
     // Save TDEE data to backend
 
-
-   const response = await fetch(`/api/users/tdee`, {
+  const API_URL = process.env.REACT_APP_API_URL;
+   const response = await fetch(`${API_URL}/api/users/tdee`, {
   method: 'PUT',  // instead of PUT
   headers: {
     'Content-Type': 'application/json',

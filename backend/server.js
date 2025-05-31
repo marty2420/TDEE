4815ac728c6aea72ceadeab95f37733f864bebdb
marty2420 <104203147+marty2420@users.noreply.github.com>
2025-05-31 13:31:01 +0800

    balik server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 // Import Routes
-const userRoutes = require('../backend/routes/userRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 // Use Routes
 app.use('/api/users', userRoutes);  
